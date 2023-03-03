3fb2e0d1074ecf42abf4acc4e097014c3e448cde
Henrik Valve <henrik.valve@kolumbus.fi>
2023-03-03 10:35:39 +0200

    EntityObject tab fix

--- src/RPG/EntityObject.java
+++ src/RPG/EntityObject.java
@@ -6,36 +6,36 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 
 public abstract class EntityObject{
-  /**Coordinates of the object*/
-  float x,y;
-  /**Width and height of the images that object uses right then*/
-  int middleX,middleY;
-  /**Does object collide with others*/
-  boolean collision=true;
-  /**Basic information sent to this constructor*/
-    EntityObject(float _x,float _y,boolean collision){
-      x=_x;
-      y=_y;
-	  this.collision=collision;
-    }
-    /**Gives sprite of given index form SpriteSet used by object*/
-    Image getSprite(int index){
-      return getSet().getSprite(index);
-    }
-    /**Gives animation of given index form SpriteSet used by object*/
-    Animation getAnimation(int index){
-      return getSet().getAnimation(index);
-    }
-    /**Objects initialization has to be put here. (Method runs at main loop)*/
-    abstract void init(GameContainer gameC,Room room);
-    /**How object is drew at screen has to be put here. (Method runs at main loop)*/
-    abstract void draw(Graphics g);
-    /**Object SpriteSet has to be here as return value*/
-    public abstract SpriteSet getSet();
-    /**Is EntityObject drawn at to the screen*/
-    public boolean isdrawn(){
-      return true;
-    }
+	/**Coordinates of the object*/
+	float x,y;
+	/**Width and height of the images that object uses right then*/
+	int middleX,middleY;
+	/**Does object collide with others*/
+	boolean collision=true;
+	/**Basic information sent to this constructor*/
+	EntityObject(float _x,float _y,boolean collision){
+		x=_x;
+		y=_y;
+		this.collision=collision;
+	}
+	/**Gives sprite of given index form SpriteSet used by object*/
+	Image getSprite(int index){
+		return getSet().getSprite(index);
+	}
+	/**Gives animation of given index form SpriteSet used by object*/
+	Animation getAnimation(int index){
+		return getSet().getAnimation(index);
+	}
+	/**Objects initialization has to be put here. (Method runs at main loop)*/
+	abstract void init(GameContainer gameC,Room room);
+	/**How object is drew at screen has to be put here. (Method runs at main loop)*/
+	abstract void draw(Graphics g);
+	/**Object SpriteSet has to be here as return value*/
+	public abstract SpriteSet getSet();
+	/**Is EntityObject drawn at to the screen*/
+	public boolean isdrawn(){
+		return true;
+	}
 
 }
 /* Henrik Valve
